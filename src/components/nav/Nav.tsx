c36ce45bf9ea0ba86a65214af7475cfc102c5a6f
Garrett Pilgrim <pilgrimgarrett@gmail.comgit config --global user.name Garrett>
2023-01-27 13:21:57 -0600

    Added components and pages

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+import styles from "./nav.module.scss";
+
+export default function Nav() {
+  return(
+    <div className={styles.container}>
+      <nav>
+        <div>
+          logo
+        </div>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/sermons">Sermons</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+};
