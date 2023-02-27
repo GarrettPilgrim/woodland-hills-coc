cad3031e09dbd7e78e77985c8746476b686860c7
Garrett Pilgrim <pilgrimgarrett@gmail.comgit config --global user.name Garrett>
2023-02-27 10:09:06 -0600

    Updated config for components

--- content/components/announcement.md
+++ content/components/announcement.md
@@ -1,8 +1,8 @@
 ---
-heading: "This is a home page announcment"
+title: "This is a home page announcment"
 subheading: "This is the text to go along with the announcement"
-backgroundimage: "/images/uploads/announcement-image.jpg"
-backgroundalt: "This is image alt text for announcement"
+image: "/images/uploads/announcement-image.jpg"
+alt: "This is image alt text for announcement"
 coverimage: "/images/uploads/announcement-image.jpg"
 coveralt: "This is image alt text for announcement"
 ---
--- src/components/announcement/Announcement.jsx
+++ src/components/announcement/Announcement.jsx
@@ -1,25 +1,25 @@
-import styles from "./announcement.module.scss"
-import Image from "next/image"
-import { attributes, react as AnnouncementText } from "@content/components/announcement.md";
+import styles from "./announcement.module.scss";
+import Image from "next/image";
+import {
+  attributes,
+  react as AnnouncementText,
+} from "@content/components/announcement.md";
 
 export default function Annoucement() {
-  let { heading, subheading, backgroundimage, backgroundalt, coverimage, coveralt } = attributes;
-  return(
-      <div className={styles.container}>
-        <div className={styles.text}>
-          <div className={styles.image}>
-            <Image
-              src={backgroundimage}
-              alt={backgroundalt}
-              fill
-            />
-          </div>
-          <h2>{heading}
+  let { title, subheading, image, alt } = attributes;
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+        <div className={styles.image}>
+          <Image src={image} alt={alt} fill />
+        </div>
+        <h2>
+          {title}
           <br />
           <span>{subheading}</span>
-          </h2>
-          <AnnouncementText />
-        </div>
+        </h2>
+        <AnnouncementText />
       </div>
-  )
+    </div>
+  );
 }
