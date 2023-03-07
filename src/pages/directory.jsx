d50e18cbe5acaa1cb87821e9a8bac4bbae6df98c
Garrett Pilgrim <pilgrimgarrett@gmail.comgit config --global user.name Garrett>
2023-03-07 10:05:36 -0600

    Button design

--- src/pages/directory.jsx
+++ src/pages/directory.jsx
@@ -2,9 +2,18 @@ import Layout from "@/components/layout";
 import Cover from "@/components/cover/cover";
 
 export default function Directory() {
+  const { user } = "hello";
+
+  if (!user || user.isLoggedIn === false) {
+    return <Layout>Loading...</Layout>;
+  }
   return (
     <Layout>
       <Cover heading={"Directory"} />
     </Layout>
   );
 }
+
+// https://dev.to/ndom91/adding-authentication-to-an-existing-serverless-next-js-app-in-no-time-with-nextauth-js-192h
+// https://next-auth.js.org/tutorials
+// Authenticate page with NextAuth and Google OAuth keys
