fd05e81b59909d0a522103c8e3ce5c96d68e9b8c
Garrett Pilgrim <pilgrimgarrett@gmail.comgit config --global user.name Garrett>
2023-03-07 09:05:53 -0600

    Added directory page

---
+++ src/pages/directory.jsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/layout";
+import Cover from "@/components/cover/cover";
+
+export default function Directory() {
+  return (
+    <Layout>
+      <Cover heading={"Directory"} />
+    </Layout>
+  );
+}
