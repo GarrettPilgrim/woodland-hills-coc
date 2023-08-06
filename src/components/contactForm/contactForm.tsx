a5d5b1a699ce297a8b9ec1a4987857b59012b60e
Garrett Pilgrim <pilgrimgarrett@gmail.comgit config --global user.name Garrett>
2023-08-06 08:21:42 -0500

    honeypot

--- src/components/contactForm/contactForm.tsx
+++ src/components/contactForm/contactForm.tsx
@@ -3,7 +3,7 @@ import styles from "./contactForm.module.scss";
 export default function ContactForm () {
   return(
     <div className={styles.container}>
-      <form name="contact" action="/success" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+      <form name="contact" action="/success" method="POST" data-netlify="true">
         <input type="hidden" name="wh-contact-form" value="contact" />
         <div className={styles.hidden}>
           <label>
